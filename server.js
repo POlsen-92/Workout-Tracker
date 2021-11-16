ddb0b2ae6231045fd455293a0e79a98811584d8a
FemBot 9000 <paige.el.olsen@gmail.com>
2021-11-16 13:23:38 -0800

    deploying

--- server.js
+++ server.js
@@ -7,7 +7,13 @@ const PORT = process.env.PORT || 3000;
 const db = require("./models");
 const app = express();
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true })
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
+  { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false 
+  })
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
