a686a45d6a75a255773d2e99714dac43b630c848
FemBot 9000 <paige.el.olsen@gmail.com>
2021-11-16 13:17:52 -0800

    Finished server routes so that range is working

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require("express");
-const mongojs = require("mongojs");
-const Mongoose = require("mongoose");
+const mongoose = require("mongoose");
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
-const Workout = require("./models/Workout");
+const db = require("./models");
 const app = express();
 
-Mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true })
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true })
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -18,33 +17,80 @@ app.use(express.static("public"));
 //ROUTES~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // api/workouts
     //CREATE ROUTES
-
-
-
-
-
-
+      app.post("/api/workouts", ({ body }, res) => {
+        db.Workout.create(body)
+          .then(workout => {
+            res.json(workout);
+          })
+          .catch(err => {
+            res.json(err);
+          });
+      })
 
     // READ ROUTES
+      app.get("/api/workouts", (req,res) => {
+        db.Workout.aggregate([
+        {
+          $match: { } 
+        },{
+          $addFields: {
+            totalDuration: { $sum: "$exercises.duration" }
+          }
+        }])
+          .then(workout => {
+            res.json(workout);
+          })
+          .catch(err => {
+            res.json(err);
+          });
+      })
 
+      app.get("/api/workouts/range", (req,res) => {
+        db.Workout.aggregate([
+          {
+            $match: {}
+          },{
+            $sort: {day: -1}
+          },{
+            $limit: 7 
+          },{
+          $addFields: {
+            totalWeight: { $sum: "$exercises.weight" },
+            totalDuration: { $sum: "$exercises.duration" }
+          }
+        }])
+        .then(workout => {
+          res.json(workout);
+        })
+        .catch(err => {
+          res.json(err);
+        });
+      })
 
+      
+      app.get("/exercise", (req, res) => {
+        res.redirect("/exercise.html");
+      });
 
-
-
+      app.get("/stats", (req, res) => {
+        res.redirect("/stats.html");
+      });
 
 
     // UPDATE ROUTES
-
-
-
-
-
-
-
-
-    //DELETE ROUTES
-
-
+      app.put("/api/workouts/:id", (req,res) => {
+        db.Workout.updateOne(
+          { _id: req.params.id },
+          { $push: { exercises: req.body } },
+          (error, success) => {
+            if (error) {
+              res.json(error);
+            } else {
+              res.json(success);
+            }
+          }
+        );
+      })
 
 
 
